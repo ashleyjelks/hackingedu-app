6abe98e9d296b6957e14e947053edfd68909d394
amandasf <amandafuhrmann@gmail.com>
2015-10-24 21:54:24 -0700

    attempting connection to postgres

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, flash, redirect, request
+from flask import Flask, render_template, flash, redirect, request, session
+from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
+import requests, json
+
 from model import connect_to_db, db, init_app, User, Class, Session
 
 
@@ -8,9 +11,21 @@ app = Flask(__name__)
 app.jinja_env.undefined = StrictUndefined
 
 app.secret_key = "MuchSecretWow"
+API_KEY = '1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb'
 
 @app.route('/')
 def index(): 
+	url = "https://api.target.com/items/v3/"
+	payload = {
+			"product_id": "081-04-0231",
+			"id_type": 'dpci', 
+			"key": '1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb',
+			}
+	data = requests.get(url, params=payload)
+	# data = json.loads(data.content)
+	print "The data: ", data
+	print "what you can do: ", data.content
+
 	return render_template('index.html')
 
 
@@ -28,9 +43,9 @@ def process_registration():
 	user_name = request.form['username']
 	password = request.form['password']
 	phone = request.form['phone']
-	tutor_id = request.form['tutor']
+	tutor_id = int(request.form['tutor_id'])
 
-	new_user = User(email=email, user_name=user_name, password=password, phone=phone, tutor_id=tutor_id)
+	new_user = User(user_name=user_name, email=email, password=password, phone=phone, tutor_id=tutor_id)
 
 	old_email = User.query.filter(User.email == email).first()
 	old_user_name = User.query.filter(User.user_name == user_name).first()
@@ -43,7 +58,7 @@ def process_registration():
 		session["user_id"] = user.user_name
 	else:
 		flash("That Username or Email is already in use")
-		return redirect('/register-form')
+		return redirect('/')
 
 	flash('Welcome %s' % user_name)
 	return redirect('/')
@@ -52,4 +67,6 @@ def process_registration():
 
 if __name__ == "__main__": 
 	app.debug = True
+	connect_to_db(app)
+	DebugToolbarExtension(app)
 	app.run()
